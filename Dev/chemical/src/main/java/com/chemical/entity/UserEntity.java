920802b2930c59d8a919e8cc166d5ca03c0aea6c
yeongmo-j <yeongmoj94@gmail.com>
2019-09-29 02:11:37 +0900

    Initialize Spring boot project

---
+++ Dev/chemical/src/main/java/com/chemical/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.chemical.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@Data
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	@Column
+	private String email;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String password;
+	
+}
