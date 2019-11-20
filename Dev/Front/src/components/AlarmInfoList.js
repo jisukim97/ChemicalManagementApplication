1d143eb246211d63834253d28ebf38ccc959df39
unknown <dmsan3@hanmail.net>
2019-11-20 21:53:16 +0900

    qwe

--- Dev/Front/src/components/AlarmInfoList.js
+++ Dev/Front/src/components/AlarmInfoList.js
@@ -2,25 +2,22 @@ import React, { Component } from 'react';
 import AlarmInfo from './AlarmInfo1';
 
 class AlarmInfoList extends Component {
-  static defaultProps = {
-    data: []
-  }
+    static defaultProps = {
+        data: [],
+        onRemove: () => console.warn('onRemove not defined'),
+    }
 
-  list = (data) => {
-    return (data.map(
-      (<AlarmInfo key={info.id} info={info}/>)
-    ))
-  }
 
-  render() {
-    const { data } = this.props;
-
-    return (
-      <div>
-        {this.list(data)}    
-      </div>
-    );
-  }
+    render() {
+        const { onRemove } = this.props;
+        return (
+            <div>
+                {this.props.data.map(info => {
+                    return <AlarmInfo key={info.id} info={info} onRemove={onRemove} />
+                })}
+            </div>
+        );
+    }
 }
 
 export default AlarmInfoList;
