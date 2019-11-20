7dca1ff2d0818d3a95ef7aabd530323b9e4e1a68
yeongmo-j <yeongmoj94@gmail.com>
2019-11-20 21:24:51 +0900

    Update entity class

--- Dev/chemical/src/main/java/com/team/chemical/entity/Lab.java
+++ Dev/chemical/src/main/java/com/team/chemical/entity/Lab.java
@@ -4,6 +4,9 @@ package com.team.chemical.entity;
 import java.util.*;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -33,16 +36,25 @@ public class Lab {
     /**
      * 
      */
+	@OneToMany(mappedBy = "user")
     private Set<User> members;
 
     /**
      * 
      */
+    @OneToMany
+    @JoinTable(name="lab_inventory",
+    		joinColumns=@JoinColumn(name="lab_id"),
+    		inverseJoinColumns=@JoinColumn(name="inventory_id"))
     private Set<Inventory> inventories;
 
     /**
      * 
      */
+    @OneToMany
+    @JoinTable(name="lab_apparatus",
+    		joinColumns=@JoinColumn(name="lab_id"),
+    		inverseJoinColumns=@JoinColumn(name="apparatus_id"))
     private Set<Apparatus> apparatus;
 
 }
--- Dev/chemical/src/main/java/com/team/chemical/entity/User.java
+++ Dev/chemical/src/main/java/com/team/chemical/entity/User.java
@@ -3,7 +3,12 @@ package com.team.chemical.entity;
 import java.time.LocalDate;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -13,40 +18,58 @@ import lombok.NoArgsConstructor;
 @Table(name="user")
 @Data
 @NoArgsConstructor
+//회원
 public class User {
     /**
      * 
      */
+	@Column
     private Integer id;
 
     /**
      * 
      */
+	@Column
     private String name;
 
     /**
      * 
      */
+	@Column
     private String email;
 
     /**
      * 
      */
+	@Column
     private String password;
 
     /**
      * 
      */
+	@Column
     private LocalDate labEnrollDate;
 
     /**
      * 
      */
+	@Column
+    @ManyToOne
+    @JoinColumn(name = "lab_id")
     private Lab myLab;
 
     /**
      * 
      */
+    @OneToMany
+    @JoinTable(name="lab_stock",
+    		joinColumns=@JoinColumn(name="lab_id"),
+    		inverseJoinColumns=@JoinColumn(name="stock_id"))
     private Set<Stock> alarms;
 
+    /**
+     * 
+     */
+	@OneToMany(mappedBy = "schedule")
+    private Set<Schedule> scheduleList;
 }
