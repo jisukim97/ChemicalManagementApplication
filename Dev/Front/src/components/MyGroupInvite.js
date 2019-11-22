95da4f7d6f1a1f0781f07465df3c9f72dc201304
Jooyoung070 <55049046+Jooyoung070@users.noreply.github.com>
2019-11-22 13:16:08 +0900

    asdf

--- Dev/Front/src/components/MyGroupInvite.js
+++ Dev/Front/src/components/MyGroupInvite.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Modal } from 'antd';
 //import { Link } from "react-router-dom";
 import { Typography } from 'antd';
 
@@ -9,6 +9,28 @@ import { Typography } from 'antd';
 const { Title } = Typography;
 
 class MyGroupInvite extends Component {
+
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
     render() {
         const { getFieldDecorator } = this.props.form;
@@ -30,11 +52,20 @@ class MyGroupInvite extends Component {
                 </Form.Item>
 
                 {/* 초대하기 버튼 */}
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="button">
-                        초대하기
-                    </Button>
-                </Form.Item>
+
+                  <Button type="primary" onClick={this.showModal}>
+                  초대하기
+                  </Button>
+                  <Modal
+                  title="초대하기"
+                  visible={this.state.visible}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+                  >
+                  <p>님을 My Lab에 초대하시겠습니까?</p>
+                  </Modal>
+
+
 
             </div>
         );
