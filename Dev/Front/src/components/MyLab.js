e92ac8c1a6da41305ccc0e7102f38acc5f1bde60
HJ123 <49524866+HJ135@users.noreply.github.com>
2019-11-23 17:03:29 +0900

    ws

--- Dev/Front/src/components/MyLab.js
+++ Dev/Front/src/components/MyLab.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { List, Typography} from 'antd'
+import { List, Typography } from 'antd'
 
 import Chemical from './Chemical';
-import Chemicaladd from './ChemicalAdd';
 import ChemicalAdd from './ChemicalAdd';
+import { array } from 'prop-types';
 
 const { Title } = Typography;
 
@@ -11,66 +11,127 @@ const { Title } = Typography;
 class MyLab extends Component {
 
     state = {
-        chemicals : [
+        chemicals: [
             {
-                nickname : "benzene 2",
-                stockId : 3,
-                chemicalName : "benzene",
-                inventory : "냉장고1"
+                nickname: "benzene 2",
+                stockId: 3,
+                chemicalName: "benzene",
+                inventory: "냉장고1",
+                stockInfo: {
+                    inventory: '냉장고1',
+                    putDate: "2019/10/31",
+                    expireDate: "2019/10/31",
+                    volume: 100,
+                    remainingVolume: 50
+                }
             },
             {
-                nickname : "acetone 1",
-                stockId : 4,
-                chemicalName : "acetone",
-                inventory : "시약장2"
-            }, 
+                nickname: "acetone 1",
+                stockId: 4,
+                chemicalName: "acetone",
+                inventory: "시약장2",
+                stockInfo: {
+                    inventory: '냉장고1',
+                    putDate: "2019/10/31",
+                    expireDate: "2019/10/31",
+                    volume: 100,
+                    remainingVolume: 50
+                }
+            },
             {
-                nickname : "something-A",
-                stockId : 5,
-                chemicalName : "something",
-                inventory : "시약장2"
-            }, 
+                nickname: "something-A",
+                stockId: 5,
+                chemicalName: "something",
+                inventory: "시약장2",
+                stockInfo: {
+                    inventory: '냉장고1',
+                    putDate: "2019/10/31",
+                    expireDate: "2019/10/31",
+                    volume: 100,
+                    remainingVolume: 50
+                }
+            },
             {
-                nickname : "something-B",
-                stockId : 6,
-                chemicalName : "something",
-                inventory : "시약장2"
-            }, 
+                nickname: "something-B",
+                stockId: 6,
+                chemicalName: "something",
+                inventory: "시약장2",
+                stockInfo: {
+                    inventory: '냉장고1',
+                    putDate: "2019/10/31",
+                    expireDate: "2019/10/31",
+                    volume: 100,
+                    remainingVolume: 50
+                }
+            },
             {
-                nickname : "something-C",
-                stockId : 7,
-                chemicalName : "something",
-                inventory : "시약장2"
-            }, 
+                nickname: "something-C",
+                stockId: 7,
+                chemicalName: "something",
+                inventory: "시약장2",
+                stockInfo: {
+                    inventory: '냉장고1',
+                    putDate: "2019/10/31",
+                    expireDate: "2019/10/31",
+                    volume: 100,
+                    remainingVolume: 50
+                }
+            },
             {
-                nickname : "something-D",
-                stockId : 8,
-                chemicalName : "something",
-                inventory : "시약장2"
+                nickname: "something-D",
+                stockId: 8,
+                chemicalName: "something",
+                inventory: "시약장2",
+                 stockInfo: {
+                    inventory: '냉장고1',
+                    putDate: "2019/10/31",
+                    expireDate: "2019/10/31",
+                    volume: 100,
+                    remainingVolume: 50
+                }
             }
         ]
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
+    }
+
+    changeVolume = (stockId, change) => {
+        const chemicalArray = [];
+
+        for(let i = 0 ; i < this.state.chemicals.length ; i++){
+            if (this.state.chemicals.stockId === stockId){
+                this.state.chemicals.remainingVolume = this.state.chemicals.remainingVolume - change 
+                chemicalArray.push(this.state.chemicals)
+            } 
+            this.setState({chemicals : chemicalArray})
+        }
+
+        console.log(chemicalArray)
+        //state에 있는 배열 탐색하면서 그거의 stockId가 stockId인거 찾기
+        //나머지들은 바로 chemicalArray에 넣어주고
+        //찾은거면 그거의 stockInfo.volume에서 change만큼을 뺴준다
+        //그리고 그걸 chemicalArray에 넣어주기
+        //탐색 끝나면 this.setState({chemicals : chemicalArray})
     }
 
     render() {
         return (
             <div>
                 {/* 약품 목록에서 각각 하나의 원소에 대한 Chemical 클래스 */}
-                <br/>
-                <center><Title style={{marginBottom : 50}}>My Lab</Title></center>
+                <br />
+                <center><Title style={{ marginBottom: 50 }}>My Lab</Title></center>
                 <List
                     grid={{ gutter: 16, column: 3 }}
                     dataSource={this.state.chemicals}
                     renderItem={item => (
                         <List.Item>
-                            <Chemical chemical={item}/>
+                            <Chemical chemical={item} />
                         </List.Item>
                     )}
                 />
-                <div style={{ marginTop : 100}}>
+                <div style={{ marginTop: 100 }}>
                     <ChemicalAdd />
                 </div>
             </div>
