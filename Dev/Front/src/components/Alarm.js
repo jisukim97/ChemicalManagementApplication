0fc5006213cf0b097b4adc41b028d5585e3b5bef
unknown <dmsan3@hanmail.net>
2019-11-26 14:23:32 +0900

    asd

--- Dev/Front/src/components/Alarm.js
+++ Dev/Front/src/components/Alarm.js
@@ -9,41 +9,97 @@ class Alarm extends Component {
     state = {
     }
     
+    
   
     constructor(props){
         super(props);
+        var information = [
+            {
+                id: 0,
+                name: 'Hexane',
+                date: '15',
+                alarmType : 1
+              },
+              {
+                id: 1,
+                name: 'Benzene',
+                date: '7',
+                alarmType : 1
+              },
+              {
+                id: 2,
+                name: 'Benzeqwene',
+                date: '10',
+                alarmType : 1
+              },
+              {
+                id: 3,
+                name: 'Hexane',
+                place : '냉장고',
+                alarmType : 2
+              },
+              {
+                 id: 4,
+                 name: 'Benzene',
+                 place : '오븐',
+                 alarmType : 2
+              },
+              {
+                 id: 5,
+                 name: 'Benzeqwene',
+                 place : '가방',  
+                 alarmType : 2
+              },
+              {
+                  id: 6,
+                  name: 'Benzenecwqwsd',
+                  use : '3개월',
+                  disease : "간암",
+                  alarmType : 3
+               },
+               {
+                  id: 7,
+                  name: 'Benzeqweneefefccv',
+                  use : '6개월',  
+                  disease : "췌장암",
+                  alarmType : 3
+               }
+        ]
         this.state = {
             type : 1,
-            information: [
-                {
-                  id: 0,
-                  name: 'Hexane',
-                  date: '15'
-                },
-                {
-                  id: 1,
-                  name: 'Benzene',
-                  date: '7'
-                },
-                {
-                  id: 2,
-                  name: 'Benzeqwene',
-                  date: '10'
-                }
-            ]
+            information: information,
+            alarm1Count : information.filter( value => value.alarmType === 1).length,
+            alarm2Count : information.filter( value => value.alarmType === 2).length,
+            alarm3Count : information.filter( value => value.alarmType === 3).length
+            
         }
     }
 
-    handleRemove = (id) => {
+    handleRemove = (id, alarmType) => {
         const { information } = this.state;
+        var type1Count = this.state.alarm1Count;
+        var type2Count = this.state.alarm2Count;
+        var type3Count = this.state.alarm3Count;
+        if (alarmType===1){
+            type1Count -= 1;
+        }
+        else if (alarmType===2){
+            type2Count -= 1;
+        }
+        else if (alarmType===3){
+            type3Count -= 1;
+        }
         const new_information = information.filter(info => info.id !==id )
         this.setState({
-            information: new_information
+            information : new_information,
+            alarm1Count : type1Count,
+            alarm2Count : type2Count,
+            alarm3Count : type3Count
         })
     }
 
 
-
+    
     makeTypeOne = () => {
         this.setState({type : 1})
     }
@@ -55,15 +111,13 @@ class Alarm extends Component {
     makeTypeThree = () => {
         this.setState({type : 3})
     }
+
+    function = (value) =>{
+        return value.alarmType 
+    }
     
     getContent = () => {
-        if (this.state.type===1){
-            return (<div><AlarmInfoList data={this.state.information} onRemove={this.handleRemove}/></div>)
-        } else if (this.state.type===2) {
-            return (<div>재고소진 알림들</div>)
-        } else {
-            return (<div>의심질병 알림들</div>)
-        }
+        return (<div><AlarmInfoList data={this.state.information.filter( value => value.alarmType === this.state.type )} onRemove={this.handleRemove}/></div>)
     }
 
 
@@ -77,17 +131,17 @@ class Alarm extends Component {
                 <Row style={{marginBottom : 30}}>
                     <Col span={8}><center>
                         <Button Button shape="circle" icon="bell" onClick={this.makeTypeOne} style={{ fontSize: '25px' }} size="large"/><br/>
-                        유효기간
+                        유효기간 {this.state.alarm1Count}
                     </center></Col>
 
                     <Col span={8}><center>
                         <Button Button shape="circle" icon="bell" onClick={this.makeTypeTwo} style={{ fontSize: '25px' }} size="large"/><br/>
-                        재고소진
+                        재고소진 {this.state.alarm2Count}
                     </center></Col>
 
                     <Col span={8}><center>
                         <Button Button shape="circle" icon="bell" onClick={this.makeTypeThree} style={{ fontSize: '25px' }} size="large"/><br/>
-                        의심질병
+                        의심질병 {this.state.alarm3Count}
                     </center></Col>
                 </Row>
 
--- Dev/Front/src/components/AlarmInfo1.js
+++ Dev/Front/src/components/AlarmInfo1.js
@@ -1,26 +1,114 @@
 import React, { Component } from 'react';
+import { Col, Button, } from 'antd';
+import { Modal } from 'antd';
+
 
 class AlarmInfo extends Component {
-  
 
-    handleRemove = () => {
-        // 삭제 버튼이 클릭되면 onRemove 에 id 넣어서 호출
-        const { info, onRemove } = this.props;
-        
-        onRemove(info.id);
-      }
-        
-    
-  render() {
-  
+  state = {
+    visible: false
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+
+  handleRemove = () => {
+    // 삭제 버튼이 클릭되면 onRemove 에 id 넣어서 호출
+    const { info, onRemove } = this.props;
+    onRemove(info.id, info.alarmType);
+  }
+
+
+  getMessage = () => {
+    const { info, onRemove } = this.props;
+    if (info.alarmType === 1) {
       return (
-        <div >
-            <div>
-                <b>{this.props.info.name}</b>의 유효기간이 <b>{this.props.info.date}</b>일 남았습니다.
-                <button onClick={this.handleRemove}> X </button>
 
-            </div>
+        <div style={{ fontSize: 18 }}>
+          <Button type="link" onClick={this.showModal} style={{ fontSize: '20px' }}><b>{this.props.info.name}</b></Button>
+          의 유효기간이 <b>{this.props.info.date}</b>일 남았습니다.
+          <Button icon="delete" onClick={this.handleRemove}></Button>
+          
+          <Modal
+            title="약품 정보"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            {/* 정보 출력  */}
+          </Modal>
         </div>
+      )
+    }
+    else if (info.alarmType === 2) {
+      return (
+
+        <div style={{ fontSize: 18 }}>
+          <b>{this.props.info.place}</b>에 있는 <Button type="link" onClick={this.showModal} style={{ fontSize: '20px' }}><b>{this.props.info.name}</b></Button>의 양이 얼마남지 않았습니다.
+                <Button icon="delete" onClick={this.handleRemove}></Button>
+
+          <Modal
+            title="약품 정보"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            {/* 정보 출력  */}
+          </Modal>
+
+        </div>
+      )
+    }
+    else {
+      return (
+
+        <div style={{ fontSize: 18 }}>
+          <Button type="link" onClick={this.showModal} style={{ fontSize: '20px' }}><b>{this.props.info.name}</b></Button>를 사용한지 <b>{this.props.info.use}</b>이 지났습니다. <b>{this.props.info.disease}</b>의 위험이 있습니다.
+              <Button icon="delete" onClick={this.handleRemove}></Button>
+
+          <Modal
+            title="약품 정보"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            {/* 정보 출력  */}
+          </Modal>
+
+        </div>
+      )
+    }
+
+  }
+
+  render() {
+
+    return (
+      <div >
+        {this.getMessage()}
+      </div>
     );
   }
 }
