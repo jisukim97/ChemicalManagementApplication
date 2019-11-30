ab2bf6ca9db2d1745e3a2dabd1223810a5819e80
Jooyoung070 <55049046+Jooyoung070@users.noreply.github.com>
2019-11-30 18:26:54 +0900

    asdf

--- Dev/Front/src/components/MyGroupWithdraw.js
+++ Dev/Front/src/components/MyGroupWithdraw.js
@@ -59,11 +59,34 @@ class MyGroupWithdraw extends Component {
     })
 
 
+
+
   }
 
 
 
+withdrawGroup =() => {
 
+
+  fetch(serverUrl + '/lab/' + getLab().id + '/' + getUser().id, { // uri 넣어주기
+    method: 'DELETE', //'GET', 'POST', 'DELETE' 등등
+    headers: { 'Content-Type': 'application/json' }, //안고쳐도 됨
+  }).then(response => {
+    if( response.status === 200){
+      //이건 정상적으로 된 경우
+      return response.json()
+    } else {
+      //이건 오류난 경우 -> 여기서 뭐뭐를 처리해 준다
+    }
+  }).then(response => {
+    //여기서 response로 온 값들을 state로 저장 하던가 해서 쓰면 됨
+    //여기서 response라는걸 제대로 쓸 수 있음
+    console.log(response) // 이걸로 개발자모드에서 어떠한 응답이 왔는지 확인 가능
+    //예를들면
+
+    //이렇게 응답받은 실제 결과를 status로 저장해 줄 수 있음
+  })
+}
 
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -82,7 +105,7 @@ class MyGroupWithdraw extends Component {
       <Modal
       title="탈퇴하기"
       visible={this.state.visible}
-      onOk={this.handleOk}
+      onOk={this.handleOk, this.withdrawGroup} 
       onCancel={this.handleCancel}
       >
       <p>{this.state.labName} 에서 정말 탈퇴 하시겠습니까?</p>
