93ee85a91a587b361c922b03218641c1f0fb912b
yeongmo-j <yeongmoj94@gmail.com>
2019-12-03 19:51:53 +0900

    Update

--- Dev/chemical/src/main/java/com/team/chemical/entity/LabRepository.java
+++ Dev/chemical/src/main/java/com/team/chemical/entity/LabRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface LabRepository extends CrudRepository<Lab, Integer>{
 	boolean existsByName(String name);
+	
+	Lab findByName(String name);
 }
