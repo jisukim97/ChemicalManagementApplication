2fa53f593bfa76a54440b9581720fdb1a097677a
jisukim97 <54925185+jisukim97@users.noreply.github.com>
2019-12-03 18:23:19 +0900

    mm

--- Dev/Front/src/components/ApparatusReservation.js
+++ Dev/Front/src/components/ApparatusReservation.js
@@ -15,7 +15,6 @@ class ApparatusReservation extends Component{
         this.props.form.validateFields((err, values)=> {
             
             if (!err) {
-                
                 var startTime = values['start']
                 var endTime = values['end']
                 var newday = this.props.todayDate;    
@@ -25,30 +24,28 @@ class ApparatusReservation extends Component{
                 yy = yy.substring(2, 4);
                 var mm = newday.getMonth() + 1
                 var dd = newday.getDate()
+                if(dd<10){ dd="0"+dd}
                 var newdayInfo = yy + mm + dd;
                 console.log(newdayInfo)
+                if(startTime >= endTime) { message.error("끝 시간이 시작 시간보다 빠릅니다.")}
+
+                else {
                 fetch('http://13.124.122.246:8080/schedule/' + getUser().id + '/' + this.props.apparatusId + '/' + newdayInfo + "/" + startTime + "/" + endTime, {
                     method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(values) //여기에다가 body 넣어주기
+                    headers: { 'Content-Type': 'application/json' }, 
+                    body: JSON.stringify(values)
                 }).then(response => {
                     if (response.status === 200) {
                         return response.json()
                     } else {
-                        console.log("기기 예약 시간 중복") 
-                        return 2
+                        message.warning('예약 시간이 중복되었습니다.');
                     }
                 }).then(response => {
-                    if (response === 2){
-                        console.log("here") 
-                    } else {
                         this.props.plusReservation(response.schedules)
-    
-                    }
                 })
             }
+        }
         });
-        //this.props.앞에서새로만든함수(response로받은 리스트)
     };
 
   
@@ -60,7 +57,7 @@ class ApparatusReservation extends Component{
                 {getFieldDecorator('start', {
                     rules: [{ required: true, message: '예약 시작 시간을 입력하세요. ' }],
                 })( 
-                    <Input placeholder="new apparatue name?" />
+                    <Input placeholder="ex: 1000 " />
                     //<center><TimePicker defaultValue={moment('12:00', this.state.format)} format={this.state.format} /></center>
                 )}
             </Form.Item>
@@ -68,14 +65,16 @@ class ApparatusReservation extends Component{
                 {getFieldDecorator('end', {
                     rules: [{ required: true, message: '예약 끝 시간을 입력하세요. ' }],
                 })(
-                    <Input placeholder="new apparatue name?" />
+                    <Input placeholder="ex: 1200" />
                     //<center><TimePicker defaultValue={moment('14:00', this.state.format)} format={this.state.format} /></center>    
                 )}
             </Form.Item>
             <Form.Item>
-                <Button type="primary" htmlType="submit" className="button">
-                    예약 하기
-                </Button>
+                <center>
+                    <Button type="primary" htmlType="submit" className="button">
+                        예약 하기
+                    </Button>
+                </center>
             </Form.Item>
         </Form>
         )
