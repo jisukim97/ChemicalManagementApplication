fa564c9cb35b75acdae2a5710b981b6310ba6f62
yeongmo-j <yeongmoj94@gmail.com>
2019-12-04 23:56:08 +0900

    Refresh files

--- Dev/Front/src/components/Alarm.js
+++ Dev/Front/src/components/Alarm.js
@@ -63,7 +63,21 @@ class Alarm extends Component {
                   alarmType : 3
                }
         ]
-
+        
+        fetch('http://13.124.122.246:8080/alarmtest', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        }).then(response=>{
+            if (response.status === 200) {
+                //이건 정상적으로 된 경우
+                
+                return response.json()
+            } else {
+                console.log(123)
+                //이건 오류난 경우 -> 여기서 뭐뭐를 처리해 준다
+            }
+        })
+        
         fetch('http://13.124.122.246:8080/alarm/' + getUser().id, {
             method: 'GET', //'GET', 'POST', 'DELETE' 등등
             headers: { 'Content-Type': 'application/json' }, //안고쳐도 됨
@@ -76,10 +90,9 @@ class Alarm extends Component {
                 //이건 오류난 경우 -> 여기서 뭐뭐를 처리해 준다
             }
         }).then(response => {
-            //여기서 response로 온 값들을 state로 저장 하던가 해서 쓰면 됨
-            //여기서 response라는걸 제대로 쓸 수 있음
+            
             console.log(response) // 이걸로 개발자모드에서 어떠한 응답이 왔는지 확인 가능
-            var list = [] //response.alarms
+            var list = response.alarms
             var information = [] 
             for (var i in list){
                 if (i.alarmType === 1) {
