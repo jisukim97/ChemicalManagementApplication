3000889fc568037c92d723709c0cb55404ceb83e
yeongmo-j <yeongmoj94@gmail.com>
2019-12-09 03:03:21 +0900

    Update design

--- Dev/Front/src/components/InventoryAdd.js
+++ Dev/Front/src/components/InventoryAdd.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Input, Modal, Button, Radio, Checkbox, Icon } from 'antd';
+import { Input, Modal, Button, Radio, Checkbox, Icon, Row, Col } from 'antd';
 
 class InventoryAdd extends Component {
 
@@ -11,7 +11,7 @@ class InventoryAdd extends Component {
         illuminance: false,
         oximeter: true,
         explosion: false,
-        plainOptions : ['채광', '산소', '폭발방지']
+        plainOptions: ['채광', '산소', '폭발방지']
     };
 
     showModal = () => {
@@ -53,33 +53,33 @@ class InventoryAdd extends Component {
 
     onChange = (checkedValues) => {
         console.log('checked = ', checkedValues);
-        if( checkedValues.some(x => x===this.state.plainOptions[0])){
+        if (checkedValues.some(x => x === this.state.plainOptions[0])) {
             this.setState({
-                illuminance : true
+                illuminance: true
             })
         } else {
             this.setState({
-                illuminane : false 
+                illuminane: false
             })
         }
 
-        if( checkedValues.some(x => x===this.state.plainOptions[1])){
+        if (checkedValues.some(x => x === this.state.plainOptions[1])) {
             this.setState({
-                oximeter : true
+                oximeter: true
             })
         } else {
             this.setState({
-                oximeter : false 
+                oximeter: false
             })
         }
 
-        if( checkedValues.some(x => x===this.state.plainOptions[2])){
+        if (checkedValues.some(x => x === this.state.plainOptions[2])) {
             this.setState({
-                explosion : true
+                explosion: true
             })
         } else {
             this.setState({
-                explosion : false 
+                explosion: false
             })
         }
 
@@ -98,25 +98,65 @@ class InventoryAdd extends Component {
 
         return (
             <span>
-                <Button type="primary" onClick={this.showModal} size="small" icon="plus" shape="circle"/>
+                <Button type="primary" onClick={this.showModal} size="small" icon="plus" shape="circle" />
                 <Modal
                     title="보관 장소 추가"
                     visible={this.state.visible}
                     onOk={this.handleOk}
                     onCancel={this.handleCancel}
                 >
-                    이름 :  <Input placeholder="보관장소 이름" onChange={this.nameChange} />
-                    온도 :  <Input placeholder="보관장소 온도" onChange={this.temperatureChange} />
-                    습도 :  <Radio.Group value={this.state.humidity} onChange={this.humidityChange}>
-                        <Radio.Button value={-1}>없음(~3%)</Radio.Button>
-                        <Radio.Button value={0}>보통</Radio.Button>
-                        <Radio.Button value={1}>높음(71%~)</Radio.Button>
-                    </Radio.Group>
-                    <br/>
-                    기타옵션 : 
-            <Checkbox.Group options={this.state.plainOptions} defaultValue={['산소']} onChange={this.onChange} />
+                    <Row style={{ marginBottom: 10 }}>
+                        <Col span={6} >
 
-                <Button onClick={this.submit} > 추가하기 </Button>
+                        </Col>
+                        <Col span={18}>
+
+                        </Col>
+                    </Row>
+
+                    <Row style={{ marginBottom: 10 }}>
+                        <Col span={6} >
+                            이름 :
+                        </Col>
+                        <Col span={18}>
+                            <Input placeholder="보관장소 이름" onChange={this.nameChange} />
+                        </Col>
+                    </Row>
+
+                    <Row style={{ marginBottom: 10 }}>
+                        <Col span={6} >
+                            온도 :
+                        </Col>
+                        <Col span={18}>
+                            <Input placeholder="보관장소 온도" onChange={this.temperatureChange} />
+                        </Col>
+                    </Row>
+
+                    <Row style={{ marginBottom: 10 }}>
+                        <Col span={6} >
+                            습도 :
+                        </Col>
+                        <Col span={18}>
+                            <Radio.Group value={this.state.humidity} onChange={this.humidityChange}>
+                                <Radio.Button value={-1}>없음(~3%)</Radio.Button>
+                                <Radio.Button value={0}>보통</Radio.Button>
+                                <Radio.Button value={1}>높음(71%~)</Radio.Button>
+                            </Radio.Group>
+                        </Col>
+                    </Row>
+
+                    <Row style={{ marginBottom: 10 }}>
+                        <Col span={6} >
+                            기타옵션 :
+                        </Col>
+                        <Col span={18}>
+                            <Checkbox.Group options={this.state.plainOptions} defaultValue={['산소']} onChange={this.onChange} />
+                        </Col>
+                    </Row>
+
+                    <div style={{ marginBottom: 10 }}>
+                        <center><Button onClick={this.submit} > 추가하기 </Button></center>
+                    </div>
                 </Modal>
             </span>
         );
